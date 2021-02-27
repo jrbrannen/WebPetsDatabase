575a427bbab75a811c934d46ce0ac2a387e80fa4
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2021-02-26 22:50:54 -0600

    added POJO and Helper Class files

---
+++ WebPetsDatabase/src/controller/PetHelper.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author Jeremy Brannen - jrbrannen
+ *CIS175
+ * Feb 26, 2021
+ */
+public class PetHelper {
+
+}
---
+++ WebPetsDatabase/src/model/Pet.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Jeremy Brannen - jrbrannen
+ *CIS175
+ * Feb 26, 2021
+ */
+public class Pet {
+
+}
